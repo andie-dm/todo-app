90b0d3c95d8a05efd3b8b4025b39c0cc428ce965
{ andie } <124734884+andie-dm@users.noreply.github.com>
2024-03-19 15:20:03 -0600

    Your Message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,32 @@ import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.
   standalone: true,
   imports: [RouterOutlet,InputButtonUnitComponent ], 
 template: `Title: {{title}}
+<ul>
+    @for( todoItem of todoList; track todoItem.title) {
+      <li>
+        {{ todoItem.title }}
+      </li>
+    }
+  </ul>
   <app-input-button-unit></app-input-button-unit>  
 `,
   styleUrl: './app.component.scss'
 })
 export class AppComponent { 
   title = 'andies list';
-
+  todoList = [
+    {title: 'install NodeJS'},
+    {title: 'install Angular CLI'},
+    {title: 'create new app'},
+    {title: 'serve app'},
+    {title: 'develop app'},
+    {title: 'deploy app'},
+    {title: 'make meals'},
+  ];
 
   constructor() { 
     this.title = 'I Love Angular';
-  }}
+  }
+
+}
  
