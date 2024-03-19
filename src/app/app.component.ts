4d1584895cfc9f200e5d3d1a541632e532c4dbb4
{ andie } <124734884+andie-dm@users.noreply.github.com>
2024-03-19 12:00:28 -0600

    firs commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet,InputButtonUnitComponent ], 
+  template: `Title : {{title}} <app-input-button-unit/>`,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'todo-list';
+  title = 'andies list';
 }
+ 
